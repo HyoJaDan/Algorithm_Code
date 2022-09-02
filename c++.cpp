284a6e1654d11443d89e34c75eec78c1d80b9a6a
전성호 <kopasd99@gmail.com>
2022-09-02 13:02:42 +0900

    2022-09-02

--- c++.cpp
+++ c++.cpp
@@ -1,47 +1,36 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#define INF 21e8
 using namespace std;
-
-int abs(int a)
-{
-  if (a < 0)
-    a = -a;
-  return a;
-}
 
 int main(void)
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  cout.tie(NULL);
+
   int n;
   cin >> n;
   vector<int> v(n);
   for (int i = 0; i < n; i++)
     cin >> v[i];
+  int k;
+  cin >> k;
   sort(v.begin(), v.end());
 
-  int minValue = INF, a, b;
-  int L = 0, R = n - 1;
-  while (L < R)
+  int ans = 0;
+  for (int i = 0; i < n - 1; i++)
   {
-    int temp = v[L] + v[R];
-    if (abs(temp) < minValue)
+    for (int j = i + 1; j < n; j++)
     {
-      minValue = abs(temp);
-      a = v[L];
-      b = v[R];
+      int now = v[i] + v[j];
+      if (now == k)
+      {
+        ans++;
+      }
+      else if (now > k)
+        break;
     }
-    if (temp > 0)
-      R--;
-    else
-      L++;
   }
 
-  if (a < b)
-    cout << a << " " << b;
-  else
-    cout << b << " " << a;
+  cout << ans;
 }
