4b1c5f915cf97b629aae14e5d6052983be19d4e7
전성호 <kopasd99@gmail.com>
2022-09-04 20:23:46 +0900

    rg

--- c++.cpp
+++ c++.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
 #include <algorithm>
-#include <math.h>
-#define ll long long
 using namespace std;
-ll arr[5000];
-ll ans[3];
+int arr[30000];
+int rawFish[3001];
+int num, d, k, c;
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
-  int num;
-  ll result = 3000000001;
-  cin >> num;
+  cout.tie(NULL);
+  int result = 0, cnt = 0;
+  cin >> num >> d >> k >> c;
   for (int i = 0; i < num; i++)
     cin >> arr[i];
-  sort(arr, arr + num);
-  for (int k = 0; k < num - 2; k++)
+  int left = 0, right = k - 1;
+  for (int i = 0; i < k; i++)
   {
-    int l = k + 1, r = num - 1;
-    while (l < r)
-    {
-      ll val = arr[k] + arr[l] + arr[r];
-      if (abs(val) < result)
-      {
-        result = abs(val);
-        ans[0] = arr[k];
-        ans[1] = arr[l];
-        ans[2] = arr[r];
-        //처리
-      }
-      if (val < 0)
-        l++;
-      else
-        r--;
-    }
+    rawFish[arr[i]]++;
+    if (rawFish[arr[i]] == 1)
+      cnt++;
   }
-  for (int i = 0; i < 3; i++)
-    cout << ans[i] << " ";
+  rawFish[c]++;
+  if (rawFish[c] == 1)
+    cnt++;
+
+  while (left < num)
+  {
+    result = max(result, cnt);
+    if (--rawFish[arr[left++]] == 0)
+      cnt--;
+    right = (right + 1) % num;
+    if (++rawFish[arr[right]] == 1)
+      cnt++;
+  }
+  cout << result;
   return 0;
 }
