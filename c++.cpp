9bd6de2837ecf278ae45dbdc4194d31a125909c5
전성호 <kopasd99@gmail.com>
2022-09-06 13:16:08 +0900

    2022-09-06

--- c++.cpp
+++ c++.cpp
@@ -1,83 +1,123 @@
 #include <iostream>
-#include <string>
+#include <stack>
 using namespace std;
-
-int GO(int L, int R, string s)
-{
-  int ans = 1;
-  while (L < R)
-  {
-    if (s[L] == s[R])
-    {
-      L++;
-      R--;
-    }
-    else
-    {
-      return 2;
-    }
-  }
-  return 1;
-}
 
 int main(void)
 {
   ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
+  stack<int> s;
+  stack<int> s2;
   int n;
   cin >> n;
+  string str;
+  cin >> str;
   for (int i = 0; i < n; i++)
   {
-    string s;
-    cin >> s;
-
-    int L = 0, R = s.size() - 1;
-    int ans = 0;
-    bool NO = false;
-    while (L < R)
+    if (str[i] == 'R')
     {
-      if (s[L] == s[R])
-      {
-        L++;
-        R--;
-      }
-      else
-      {
-        if (ans == 0)
-        {
-          if (s[L] == s[R - 1] && s[L + 1] == s[R])
-          {
-            int a = GO(L + 1, R, s);
-            int b = GO(L, R - 1, s);
-            a > b ? cout << b << '\n' : cout << a << '\n';
-            NO = true;
-            break;
-          }
-          if (s[L] == s[R - 1])
-            R--;
-          else if (s[L + 1] == s[R])
-            L++;
-          else
-          {
-            cout << 2 << '\n';
-            NO = true;
-            break;
-          }
-          ans++;
-        }
-        else
-        {
-          cout << 2 << '\n';
-          NO = true;
-          break;
-        }
-      }
+      s.push(1);
+      s2.push(1);
     }
-    if (NO)
-      continue;
     else
-      cout << ans << '\n';
+    {
+      s.push(0);
+      s2.push(0);
+    }
   }
+  int ans = 0, ans2 = 0;
+  while (!s.empty())
+  {
+    if (s.top())
+      s.pop();
+    else
+      break;
+  }
+  while (!s.empty())
+  {
+    if (s.top() == 0)
+    {
+    }
+    else
+    {
+      ans++;
+    }
+    s.pop();
+  }
+
+  while (!s2.empty())
+  {
+    if (!s2.top())
+      s2.pop();
+    else
+      break;
+  }
+  while (!s2.empty())
+  {
+    if (s2.top() == 1)
+    {
+    }
+    else
+    {
+      ans2++;
+    }
+    s2.pop();
+  }
+  stack<int> s3;
+  stack<int> s4;
+  for (int i = n - 1; i >= 0; i--)
+  {
+    if (str[i] == 'R')
+    {
+      s3.push(1);
+      s4.push(1);
+    }
+    else
+    {
+      s3.push(0);
+      s4.push(0);
+    }
+  }
+
+  int ans3 = 0, ans4 = 0;
+  while (!s3.empty())
+  {
+    if (s3.top())
+      s3.pop();
+    else
+      break;
+  }
+  while (!s3.empty())
+  {
+    if (s3.top() == 0)
+    {
+    }
+    else
+    {
+      ans3++;
+    }
+    s3.pop();
+  }
+
+  while (!s4.empty())
+  {
+    if (!s4.top())
+      s4.pop();
+    else
+      break;
+  }
+  while (!s4.empty())
+  {
+    if (s4.top() == 1)
+    {
+    }
+    else
+    {
+      ans4++;
+    }
+    s4.pop();
+  }
+  int x, y;
+  ans < ans2 ? x = ans : x = ans2;
+  ans3 < ans4 ? y = ans3 : y = ans4;
+  x < y ? cout << x : cout << y;
 }
