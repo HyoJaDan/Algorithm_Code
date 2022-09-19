d1676b2e9370f5b26dfd11a47190632c823402e1
전성호 <kopasd99@gmail.com>
2022-09-19 10:43:49 +0900

    2022-09-19

--- c++.cpp
+++ c++.cpp
@@ -1,82 +1,44 @@
 #include <iostream>
 #include <vector>
+#define ll long long
 using namespace std;
+
+vector<ll> v;
+ll n, answer = 0;
+void run(ll now, ll value, ll ans) // now= 지금 몇번쨰인지 (i계산) value는 현재 더한값 ans=4rodlswl
+{
+  if (ans == 4)
+  {
+    if (now == v.size())
+      answer++;
+    else
+      return;
+  }
+  ll nowSum = 0;
+  for (auto i = now; i < n; i++)
+  {
+    nowSum += v[i];
+    if (nowSum == value)
+      run(i + 1, value, ans + 1);
+  }
+}
+
 int main(void)
 {
-  int k;
-  cin >> k;
-  int z = 2 * k;
-  int n = 1;
-  while (k--)
-    n *= 2;
-  vector<vector<int>> ans(n, vector<int>(n, 0));
-  int x = 0, y = 0, a = n / 2, b = n / 2;
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll a, now = 0;
+  cin >> n;
 
-  for (int i = 0; i < z; i++)
+  for (auto i = 0; i < n; i++)
   {
-    char now;
-    cin >> now;
-    if (now == 'R' || now == 'L')
-    {
-      if (now == 'R')
-        x += a;
-      a /= 2;
-    }
-    else if (now == 'D' || now == 'U')
-    {
-      if (now == 'D')
-        y += b;
-      b /= 2;
-    }
+    cin >> a;
+    v.push_back(a);
   }
-  int hole;
-  cin >> hole;
-  while (x >= 2)
-    x -= 2;
-  while (y >= 2)
-    y -= 2;
-
-  for (int i = 0; i < n; i++)
+  for (auto i = 0; i < n - 2; i++)
   {
-    int q;
-    if (hole >= 2)
-      q = hole - 2;
-    else
-      q = hole + 2;
-    for (int j = 0; j < n; j++)
-    {
-      if (i % 2 == y)
-      {
-        if (j % 2 == x)
-          ans[i][j] = hole;
-        else
-        {
-          if (hole % 2 == 1)
-            ans[i][j] = hole - 1;
-          else
-            ans[i][j] = hole + 1;
-        }
-      }
-      else
-      {
-        if (j % 2 == x)
-          ans[i][j] = q;
-        else
-        {
-          if (q % 2 == 1)
-            ans[i][j] = q - 1;
-          else
-            ans[i][j] = q + 1;
-        }
-      }
-    }
+    now += v[i];
+    run(i + 1, now, 1);
   }
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      cout << ans[i][j] << " ";
-    }
-    cout << endl;
-  }
+  cout << answer;
 }
