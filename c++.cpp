816860296691e5b134c8623f068a43184044d5db
전성호 <kopasd99@gmail.com>
2024-02-03 16:07:10 +0800

    2024 02 03

--- c++.cpp
+++ c++.cpp
@@ -1,47 +1,87 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-const int MAX = 10000;
-bool isPrime[MAX + 5] = {false};
-
-void getPrime()
+void MergeData(vector<int> &v, int left, int mid, int right)
 {
-    for (int i = 2; i <= MAX;i++)
-        isPrime[i] = true;
+	int first = left;
+	int second = mid + 1;
+	int i;
 
-    for (int i = 2; i <= MAX;i++)
-    {
-        if(isPrime[i]==false)
-            continue;
+	int tmpIndex = left;
+	// 정렬을 위한 공간 미리 선언 필요
+	vector<int> tmp(v.size());
 
-        for (int j = i + i; j <= MAX; j += i)
-        {
-            isPrime[j] = false;
-        }
+	// 인덱스를 증가해 가면서 값을 비교하고, 데이터에 넣는다.
+	while (first <= mid && second <= right)
+	{
+	   //내림차순은 <= 를 >로 바꾸면 된다
+		if (v[first] <= v[second])
+		{
+			tmp[tmpIndex] =v[first];
+			first++;
+		}
+		else
+		{
+			tmp[tmpIndex] = v[second];
+			second++;
+		}
+		tmpIndex++;
+	}
+
+	// 첫 번째 데이터에 있는 값만 다 옮긴 경우
+	if (first > mid)
+	{
+		// 두 번째 데이터에 있는 값을 그대로 옮긴다.
+		for (int i = second; i <= right; i++, tmpIndex++)
+		{
+			tmp[tmpIndex] = v[i];
+		}
+	}
+	// 두 번째 데이터에 있는 값만 다 옮긴 경우
+	else
+	{
+		// 첫 번째 데이터에 있는 값을 그대로 옮긴다.
+		for (int i = first; i <= mid; i++, tmpIndex++)
+		{
+			tmp[tmpIndex] = v[i];
+		}
+	}
+	// 정렬된 데이터 복사.
+	for (int i = left; i <= right; i++)
+	{
+		v[i] = tmp[i];
+	}
+}
+int ans = 0;
+int ansMerge = 0;
+void MergeSort(vector<int> &v, int left, int right)
+{
+    ansMerge++;
+    int mid;
+    if(left<right)
+    {   
+        //지금 있는 배열 : 0, 1, 2, 3 
+        // 1. 0,1 비교, 
+        //2. 2,3 비교 
+        // 3. 12, 34비교
+        mid = (left + right) / 2;
+        MergeSort(v, left, mid);
+        MergeSort(v, mid + 1, right);
+
+        MergeData(v, left, mid, right);
+        ans++;
     }
 }
 
-void solve()
-{
-    int n,inputData;
-    cin >> n;
-    while(n--)
-    {
-        cin >> inputData;
-        for (int i = inputData / 2; i >= 2;i--)
-        {
-            if(isPrime[i] && isPrime[inputData-i])
-            {
-                cout << i << " " << inputData-i << endl;
-                break;
-            }
-        }
-    }
-}
+int main() {
+    vector<int> v;
+    v.push_back(8);v.push_back(7);v.push_back(6);v.push_back(5);
+    v.push_back(3);/* v.push_back(2);v.push_back(1);v.push_back(4); */
 
-int main(void)  
-{
-    getPrime();
-    solve();
+	// right에는 크기가 아니라, 마지막 인덱스 값 전달.
+	MergeSort(v,0,v.size()-1);
+    cout << ans;
+    cout << endl
+         << ansMerge;
 }
