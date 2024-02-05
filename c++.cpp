7af020a13789f0ddf7fc4ac29ba7ea2492c78016
전성호 <kopasd99@gmail.com>
2024-02-05 14:38:49 +0800

    2024 02 05

--- c++.cpp
+++ c++.cpp
@@ -2,86 +2,80 @@
 #include <vector>
 using namespace std;
 
-void MergeData(vector<int> &v, int left, int mid, int right)
+int map[1030][1030] = {0};
+int n;
+
+void input()
 {
-	int first = left;
-	int second = mid + 1;
-	int i;
-
-	int tmpIndex = left;
-	// 정렬을 위한 공간 미리 선언 필요
-	vector<int> tmp(v.size());
-
-	// 인덱스를 증가해 가면서 값을 비교하고, 데이터에 넣는다.
-	while (first <= mid && second <= right)
-	{
-	   //내림차순은 <= 를 >로 바꾸면 된다
-		if (v[first] <= v[second])
-		{
-			tmp[tmpIndex] =v[first];
-			first++;
-		}
-		else
-		{
-			tmp[tmpIndex] = v[second];
-			second++;
-		}
-		tmpIndex++;
-	}
-
-	// 첫 번째 데이터에 있는 값만 다 옮긴 경우
-	if (first > mid)
-	{
-		// 두 번째 데이터에 있는 값을 그대로 옮긴다.
-		for (int i = second; i <= right; i++, tmpIndex++)
-		{
-			tmp[tmpIndex] = v[i];
-		}
-	}
-	// 두 번째 데이터에 있는 값만 다 옮긴 경우
-	else
-	{
-		// 첫 번째 데이터에 있는 값을 그대로 옮긴다.
-		for (int i = first; i <= mid; i++, tmpIndex++)
-		{
-			tmp[tmpIndex] = v[i];
-		}
-	}
-	// 정렬된 데이터 복사.
-	for (int i = left; i <= right; i++)
-	{
-		v[i] = tmp[i];
-	}
-}
-int ans = 0;
-int ansMerge = 0;
-void MergeSort(vector<int> &v, int left, int right)
-{
-    ansMerge++;
-    int mid;
-    if(left<right)
-    {   
-        //지금 있는 배열 : 0, 1, 2, 3 
-        // 1. 0,1 비교, 
-        //2. 2,3 비교 
-        // 3. 12, 34비교
-        mid = (left + right) / 2;
-        MergeSort(v, left, mid);
-        MergeSort(v, mid + 1, right);
-
-        MergeData(v, left, mid, right);
-        ans++;
+    cin >> n;
+    for (int i = 0; i < n;i++)
+    {
+        for (int j = 0; j < n;j++)
+        {
+            cin >> map[i][j];
+        }
     }
 }
 
-int main() {
-    vector<int> v;
-    v.push_back(8);v.push_back(7);v.push_back(6);v.push_back(5);
-    v.push_back(3);/* v.push_back(2);v.push_back(1);v.push_back(4); */
+int findSecondLargeNumber(int fromA, int toA, int fromB, int toB){
+    int firstNumber=-11111;
+    int secondNumber=-11111;
+    for (int i = fromA; i <= toA;i++)
+    {
+        for (int j = fromB; j <= toB;j++)
+        {
+            if (map[i][j] >= firstNumber )
+            {
+                secondNumber = firstNumber;
+                firstNumber = map[i][j];
+            }
+            else if (map[i][j] < firstNumber && map[i][j] > secondNumber)
+            {
+                secondNumber = map[i][j];
+            }
+        }
+    }
+    return secondNumber;
+}
 
-	// right에는 크기가 아니라, 마지막 인덱스 값 전달.
-	MergeSort(v,0,v.size()-1);
-    cout << ans;
-    cout << endl
-         << ansMerge;
+void divideAndConquer(vector<vector<int>> &newMap, int leftA, int rightA, int leftB, int rightB)
+{
+    if (rightA - leftA == 1)
+    {
+        int secondNumber=findSecondLargeNumber(leftA, rightA, leftB, rightB);
+        newMap[rightA / 2][rightB / 2] = secondNumber;
+    }
+    else
+    {
+        int midA = (leftA + rightA) / 2;
+        int midB = (leftB + rightB) / 2;
+
+        divideAndConquer(newMap,leftA, midA, leftB, midB);
+        divideAndConquer(newMap,leftA, midA, midB+1, rightB);
+        divideAndConquer(newMap,midA + 1, rightA, leftB, midB);
+        divideAndConquer(newMap,midA + 1, rightA, midB + 1, rightB);
+    }
+}
+void mapInitializer(){
+    int newMapSize = n;
+    while(newMapSize!=1)
+    {
+        newMapSize /= 2;
+        vector<vector<int>> newMap(newMapSize, vector<int>(newMapSize, 0));
+        divideAndConquer(newMap, 0, newMapSize * 2-1, 0, newMapSize * 2-1);
+        for (int i = 0; i < newMapSize;i++)
+        {
+            for (int j = 0; j < newMapSize;j++)
+            {
+                map[i][j] = newMap[i][j];
+            }
+        }
+    }
+}
+
+int main(void)
+{
+    input();
+    mapInitializer();
+    cout << map[0][0];
 }
